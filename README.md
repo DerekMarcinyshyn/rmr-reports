10ed02a7bc202b5eb17bfa030c87b8089829d219
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2012-12-07 22:25:59 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Revelstoke Mountain Resort Reports
+
+**Contributors**:
+
+* Derek Marcinyshyn [derek.marcinyshyn.com](http://derek.marcinyshyn.com)
+* Ian Houghton [IHoughton@revelstokemountainresort.com](IHoughton@revelstokemountainresort.com)
+
+* **Version**: 1.0
+* **Requires at least**: 3.0
+* **Tested up to**: 3.5
+* **License**: GPLv2
+
+#Description
+
+Revelstoke Mountain Resort Reports is a WordPress plugins displaying weather, snow, grooming and lift status updates.
+
+# Features
+
+Widgets and page views via shortcodes
+
---
+++ rmr-reports.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package RMR Reports
+ * @since   December 7, 2012
+ */
+/*
+Plugin Name: RMR Reports
+Plugin URI: https://github.com/rmr-reports/
+Description: Revelstoke Mountain Resort Reports is a WordPress plugins displaying weather, snow, grooming and lift status updates.
+Author: Derek Marcinyshyn
+Author URI: http://derek.marcinyshyn.com
+Version: 1.0
+License: GPLv2
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
